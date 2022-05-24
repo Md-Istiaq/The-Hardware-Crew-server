418680a4fd84734467e5ed95e0dbce833ea60690
Md-Istiaq <mdistiaq102@outlook.com>
2022-05-25 00:36:30 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,42 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const { MongoClient, ServerApiVersion ,ObjectId } = require('mongodb');
+const port = process.env.PORT || 5000
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4utyu.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const partsCollection = client.db('assignment12').collection('parts')
+
+        app.get('/parts' , async(req,res) =>{
+            const query = {}
+            const cursour =  partsCollection.find(query)
+            const parts = await cursour.toArray()
+            res.send(parts)
+        })
+
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
+
+app.get('/' , (req,res) =>{
+    res.send("Running assignment12 server")
+})
+
+app.listen(port, () =>{
+    console.log("Listening to port",port)
+})
